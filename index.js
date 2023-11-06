9be120a70cc7d25b09eb4dd89bba75f87477cd85
m104 <m104-contact@gmail.com>
2023-11-06 19:09:06 +0100

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  * @description This is the main file of the application.
  */
 
-//github shinaningans s
+//github shinaningans
 /* ----------------------------------------------- */
 /*           Intialisation des requires            */
 /* ----------------------------------------------- */
