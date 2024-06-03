55253927d52dc6356344339675ea03ea6d9677a5
Astyell <etoiledeplante@gmail.com>
2024-06-03 11:10:05 +0200

    V1.0.0 Ajout test.js

---
+++ fctAux.js
@@ -0,0 +1,38 @@
+/** fctAux.js
+ * @author Astyell
+ * @version 1.0.0 - 02/06/2024
+ * @date 02/06/2024 
+ * @description Fonction utilisée dans plusieurs commandes différentes
+ */
+
+function getDateFormate() 
+{
+	// Date au format AAAA-MM-JJ
+	let dateAjd = new Date();
+
+	// Formatage de la data pour un format correct
+	let dateFormate = dateAjd.getFullYear() + "-";
+
+	// Si le mois est < 10 il s'écrira "2" hors nous cherchons à avoir "02"
+	(dateAjd.getMonth() + 1) < 10 ? dateFormate += "0" + (dateAjd.getMonth() + 1) : dateFormate += (dateAjd.getMonth() + 1);
+
+	dateFormate += "-" + dateAjd.getDate();
+	
+	return dateFormate;
+}
+
+function getTauxCapture() 
+{
+	let random = Math.floor(Math.random() * 100 );
+	let tauxCapture = -1;
+	
+	if      (random <  40)                { tauxCapture = 40; } 
+	else if (random >= 40 && random < 70) { tauxCapture = 30; } 
+	else if (random >= 70 && random < 90) { tauxCapture = 20; } 
+	else if (random >= 90 && random < 95) { tauxCapture =  5; } 
+	else if (random >= 95 && random < 98) { tauxCapture =  3; } 
+	else if (random >= 98)                { tauxCapture  = 2; } 
+	else                                  { tauxCapture = -1; }
+
+	return tauxCapture;
+}
--- test.js
+++ test.js
@@ -1,8 +1,30 @@
-const mysql = require("./lib/database/init").test
+const mysql = require('mysql2');
+const config = require('./config.json');
 
+const connection = mysql.createConnection(config.sql);
 
-mysql.query(`SELECT * FROM PC WHERE Id_DresseurAct = 826900077120978996 AND id_Pokemon = 1 AND estShiny = 0;`, (err, cb) => {
-    console.error(err)
-    console.log(cb)
-    process.exit()
-})
+connection.connect((err) => {
+    if (err) {
+        console.error('Erreur de connexion à la base de données:', err);
+        return;
+    }
+    console.log('Connecté à la base de données');
+
+    // Ajoutez ici une requête de test pour vérifier que la base de données fonctionne
+    connection.query('SELECT * FROM Utilisateur', (error, results, fields) => {
+        if (error) {
+            console.error('Erreur lors de l\'exécution de la requête:', error);
+            return;
+        }
+        console.log('Le résultat de la requête est:', results[0]);
+
+        // Fermez la connexion après avoir vérifié la requête
+        connection.end((err) => {
+            if (err) {
+                console.error('Erreur lors de la fermeture de la connexion:', err);
+            } else {
+                console.log('Connexion fermée avec succès');
+            }
+        });
+    });
+});
